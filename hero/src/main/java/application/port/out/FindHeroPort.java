85170003ab8091d7738027004702d0fa2d3b4ede
ViniBelo <vinialvesbelo@outlook.com>
2023-12-17 21:12:22 -0300

    Nothing

--- hero/src/main/java/application/port/out/FindHeroPort.java
+++ hero/src/main/java/application/port/out/FindHeroPort.java
@@ -6,6 +6,6 @@ import java.sql.SQLException;
 import java.util.UUID;
 
 public interface FindHeroPort {
-    HeroDTO findById(UUID id) throws SQLException;
-    HeroDTO findByName(String name) throws SQLException;
+    HeroDTO findById(UUID id);
+    HeroDTO findByName(String name);
 }
